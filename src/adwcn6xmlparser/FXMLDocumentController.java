463467eefbb79df7021ac90546463037d985a2a5
drewwalters96 <drewwalters96@gmail.com>
2017-04-15 22:49:11 -0500

    refactor

--- src/adwcn6xmlparser/FXMLDocumentController.java
+++ src/adwcn6xmlparser/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package adwcn6xmlparser;
 
 import java.io.File;
@@ -16,10 +11,6 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-/**
- *
- * @author drew
- */
 public class FXMLDocumentController implements Initializable {
     
     @FXML
--- src/adwcn6xmlparser/XMLParser.java
+++ src/adwcn6xmlparser/XMLParser.java
@@ -1,31 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Created by Drew Walters
+//
+// An event-driven XML parser developed for CS4330
+// that creates a DOM representation of an XML 
+// document using the Java SAX parser.
 package adwcn6xmlparser;
 
 import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 import java.util.Stack;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.Attributes;
 
-/**
- *
- * @author drew
- */
 public class XMLParser {
-  
     public static XMLNode parse(File file) {
         XMLNode documentRoot = null;
         Stack<XMLNode> stack = new Stack<>();
